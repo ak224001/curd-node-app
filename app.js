5929ca1ce76c196878de1ce4ca8a971b3cdc180f
ak224001 <31074008+ak224001@users.noreply.github.com>
2020-07-29 20:40:58 +0530

    change port number

--- app.js
+++ app.js
@@ -21,12 +21,11 @@ app.use("/", Employeeroutes);
 
 // var port = process.env.Port || 5000;
 
-
 var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
+// var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
-app.listen(server_port, server_host, function () {
-  console.log('Listening on port %d', server_port);
+app.listen(server_port, function () {
+  console.log("Listening on port %d", server_port);
 });
 
 // app.listen(port, () => {
